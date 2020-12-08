c4fc3d6eb440421f7af899a699cea6b43c956ebf
Aaron Saksik <aaron.saksik@ynov.com>
2020-12-08 17:13:18 +0100

    Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   resources :users
+  get    '/sign_in',    to: 'sessions#sign_in_form'
+  post   '/sign_in',    to: 'sessions#sign_in'
+  delete '/sign_out',   to: 'sessions#sign_out'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
